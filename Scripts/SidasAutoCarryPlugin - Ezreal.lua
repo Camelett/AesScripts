5402d3310daa55c05b6d0d5cab2aca0d014271f8
Puze <puze997@gmail.com>
2013-10-18 15:00:53 +0300

    Added Revamped support

--- Scripts/SidasAutoCarryPlugin - Ezreal.lua
+++ Scripts/SidasAutoCarryPlugin - Ezreal.lua
@@ -1,28 +1,45 @@
 -- Variables --
 local Target
 local Config = AutoCarry.PluginMenu
+local IsReborn = false
 
 -- Skills information --
-local QRange, WRange, RRange = 1200, 1050, 2000
+local QRange, WRange, RRange = 1100, 1050, 2000
 local QSpeed, WSpeed, RSpeed = 2.0, 1.6, 2.0
 local QDelay, WDelay, RDelay = 251, 250, 1000
 local QWidth, WWidth, RWidth = 70, 90, 150
 
 -- Skills Table --
-local SkillQ = AutoCarry.Skills:NewSkill(false, _Q, QRange, "Mystic Shot", AutoCarry.SPELL_LINEAR_COL, 0, false, false, QSpeed, QDelay, QWidth, true)
-local SkillW = AutoCarry.Skills:NewSkill(false, _W, WRange, "Essence Flux", AutoCarry.SPELL_LINEAR, 0, false, false, WSpeed, WDelay, WWidth, false)
-local SkillR = AutoCarry.Skills:NewSkill(false, _R, RRange, "Trueshot Barrage", AutoCarry.SPELL_LINEAR, 0, false, false, RSpeed, RDelay, RWidth, false)
+if IsReborn then
+	SkillQ = AutoCarry.Skills:NewSkill(false, _Q, QRange, "Mystic Shot", AutoCarry.SPELL_LINEAR_COL, 0, false, false, QSpeed, QDelay, QWidth, true)
+	SkillW = AutoCarry.Skills:NewSkill(false, _W, WRange, "Essence Flux", AutoCarry.SPELL_LINEAR, 0, false, false, WSpeed, WDelay, WWidth, false)
+	SkillR = AutoCarry.Skills:NewSkill(false, _R, RRange, "Trueshot Barrage", AutoCarry.SPELL_LINEAR, 0, false, false, RSpeed, RDelay, RWidth, false)
+else
+	SkillQ = {spellKey = _Q, range = QRange, speed = QSpeed, delay = QDelay, width = QWidth, minions = true }
+    SkillW = {spellKey = _W, range = WRange, speed = WSpeed, delay = WDelay, width = WWidth, minions = false }
+    SkillR = {spellKey = _R, range = RRange, speed = RSpeed, delay = RDelay, width = RWidth, minions = false }
+end
 
 -- Plugin functions --
 function PluginOnLoad()
-	AutoCarry.Crosshair:SetSkillCrosshairRange(RRange)
-	AutoCarry.Skills:DisableAll()
+	if AutoCarry.Skils then 
+		IsReborn = true 
+	else 
+		IsReborn = false 
+	end
+
+	if IsReborn then
+		AutoCarry.Crosshair:SetSkillCrosshairRange(RRange)
+		AutoCarry.Skills:DisableAll()
+	else
+		AutoCarry.SkillsCrosshair.range = RRange
+	end
 
 	Menu()
 end
 
 function PluginOnTick()
-	Target = AutoCarry.Crosshair:GetTarget()
+	Target = GetTarget()
 
 	Combo()
 	Harass()
@@ -40,7 +57,7 @@ end
 
 -- Spells funtions --
 function Combo()
-	if AutoCarry.Keys.AutoCarry then
+	if AutoCarry.MainMenu.AutoCarry then
 		CastQ()
 		CastW()
 		CastR()
@@ -48,21 +65,33 @@ function Combo()
 end
 
 function Harass()
-	if AutoCarry.Keys.MixedMode then
+	if AutoCarry..MixedMode then
 		CastQ()
 		CastW()
 	end
 end
 
 function CastQ()
-	if Target ~= nil and GetDistance(Target) < QRange or Config.ComboOptions.ComboQ or Config.HarassOptions.HarassQ then
-		SkillQ:Cast(Target)
+	if Target ~= nil and GetDistance(Target) < QRange then
+		if Config.ComboOptions.ComboQ or Config.HarassOptions.HarassQ then
+			if IsReborn then	
+				SkillQ:Cast(Target)
+			else
+				AutoCarry.CastSkillshot(SkillQ, Target)
+			end
+		end
 	end
 end
 
 function CastW()
-	if Target ~= nil and GetDistance(Target) < WRange or Config.ComboOptions.ComboW or Config.HarassOptions.HarassW then
-		SkillW:Cast(Target)
+	if Target ~= nil and GetDistance(Target) < WRange then
+		if Config.ComboOptions.ComboW or Config.HarassOptions.HarassW then
+			if IsReborn then	
+				SkillW:Cast(Target)
+			else
+				AutoCarry.CastSkillshot(SkillW, Target)
+			end
+		end
 	end
 end
 
@@ -71,10 +100,27 @@ function CastR()
 		RDmg = getDmg("R", Target, myHero)
 		if GetDistance(Target) < RRange and RDmg > Target.health then
 			if Config.ComboOptions.ComboR or Config.FinisherOptions.FinisherR then
-				SkillR:Cast(Target)
+				if IsReborn then	
+					SkillR:Cast(Target)
+				else
+					AutoCarry.CastSkillshot(SkillR, Target)
+				end
 			end
 		end
 	end
+end
+
+-- Other functions
+function Farm()
+
+end
+
+function GetTarget()
+    if IsReborn then
+        return AutoCarry.Crosshair:GetTarget()
+    else
+        return AutoCarry.GetAttackTarget()
+    end
 end
 
 -- Menu --
